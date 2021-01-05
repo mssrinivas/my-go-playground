b2453c7a3b6eafca6bc9f5f36ed047cce00a3446
M.S.Srinivas <mudambiseshadri.srinivas@sjsu.edu>
2021-01-05 02:17:15 +0000

    Added Waitgroups example

---
+++ waitgroups.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func testWaitGroups(s string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 1000)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		testWaitGroups("go is hard myaaannnn!")
+		wg.Done()
+	}()
+
+  go func() {
+    for i:=0;i<10;i++ {
+	  	time.Sleep(time.Millisecond * 100)
+    }
+    testWaitGroups("concurrency != parallelism")
+    wg.Done()
+	}()
+
+	wg.Wait()
+}
